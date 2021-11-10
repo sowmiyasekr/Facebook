1c49c4aa884e0342a4e3c8e7fa3748a6b1aae13f
sowmiyagreens96 <sowmyasekar96@gmail.com>
2021-11-10 11:11:01 +0530

    password functionality

--- src/git/org/Login.java
+++ src/git/org/Login.java
@@ -1,5 +1,11 @@
 package git.org;
 
 public class Login {
+	public static void password() {
+		 System.out.println("password functionality");
+			}
+			public static void main(String[] args) {
+				password();
+			}
 
 }
