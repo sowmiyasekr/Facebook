557e8c2db08a3fb7406b76501bae8752b32e046d
sowmiyagreens96 <sowmyasekar96@gmail.com>
2021-11-10 10:59:17 +0530

    username functionality

--- src/git/org/Login.java
+++ src/git/org/Login.java
@@ -1,5 +1,11 @@
 package git.org;
 
 public class Login {
+	public static void username() {
+ System.out.println("username functionality");
+	}
+	public static void main(String[] args) {
+		username();
+	}
 
 }
